ababcf96b726a85df9d73b67b80a8bb5536c58ae
Stewart X Addison <sxa@redhat.com>
2023-06-08 16:16:49 +0100

    Fix regex to allow for tagged ea builds
    Signed-off-by: Stewart X Addison <sxa@redhat.com>

--- sbin/Release.sh
+++ sbin/Release.sh
@@ -41,7 +41,7 @@ timestampRegex="[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}-[[:digit:]]{2}-[[:d
 #      OpenJDK                 -debugimage aarch64_       linux_         hotspot_         2023-02-16-12-32                .tar.gz
 #
 #             (version     )  (type                                                                                           ) (arch           ) (os             ) (variant         ) (timestamp     or version      )  (extension          )
-regex="OpenJDK([[:digit:]]*)U?(-jre|-jdk|-debugimage|-static-libs-glibc|-static-libs|-static-libs-musl|-testimage|-jdk-sources)_([[:alnum:]\-]+_)?([[:alnum:]\-]+_)?([[:alnum:]\-_]+_)?([[:digit:]\-]+|[[:alnum:]\._]+)\.(tar\.gz|zip|pkg|msi)";
+regex="OpenJDK([[:digit:]]*)U?(-jre|-jdk|-debugimage|-static-libs-glibc|-static-libs|-static-libs-musl|-testimage|-jdk-sources)_([[:alnum:]\-]+_)?([[:alnum:]\-]+_)?([[:alnum:]\-_]+_)(?([[:digit:]\-\.\+]+|[[:alnum:]\._]+)\.(tar\.gz|zip|pkg|msi)";
 
 regexArchivesOnly="${regex}$";
 
